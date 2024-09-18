e2b1544fd6d3dd9dab6d94171a89934cc2195aa3
MartiresValdez17 <61474696+Sonny217@users.noreply.github.com>
2024-09-17 21:01:50 -0400

    Add files via upload

---
+++ CalcW.py
@@ -0,0 +1,7 @@
+#calculate the weight of an object
+#weight = mass * gravity
+
+mass =float(input("Enter the mass(KG) of the object:"))
+weight = mass*9.8
+print(f"The weight of the object is:{round(weight)} Newtons")
+#the round is optional
